392c3cb7877ba6ae1db240e1b360b0e8914130cd
WangHanChi <hank1122334455667788990@gmail.com>
2022-12-16 21:48:23 +0800

    Modify the UseFFT.c

--- FFT/UseFFT.c
+++ FFT/UseFFT.c
@@ -9,28 +9,84 @@ int main(int argc, char **argv)
     while (1)
     {
         printf("Do yot want to calculate FFT ? [y/n]\n>");
-        int num, check_num;
-        char cont, c;
-        scanf("%c", &cont);
+        int num = 20, check_num;
+        char cont, c, order;
+        scanf("%s", &cont);
         if (cont != 'y')
         {
             printf("Exit the program\n");
             break;
         }
-
-        printf("Please Enter the Point numbers you want?\n>");
-        scanf("%d", &num);
-        check_num = num;
-        while ((check_num & (check_num - 1)))
-        {
-            check_num++;
-        }
-        num = check_num;
         Complex *test = malloc(sizeof(Complex) * num);
-        for (int i = 0; i < num; i++)
+        printf("Do you want to get ordering sequence by auto? [y/n]\n>");
+        scanf("%s", &order);
+        printf("%c", order);
+        // c = getchar();
+
+        if (order == 'y')
         {
-            test[i].Re = i;
+            printf("Please Enter the Point numbers you want?\n>");
+            scanf("%d", &num);
+            c = getchar();
+            check_num = num;
+            while ((check_num & (check_num - 1)))
+            {
+                check_num++;
+            }
+            if (num != check_num)
+            {
+                printf("The number you input is not 2^n , so I fill it to satisfy 2^n\nthe num will be %d\n\n", check_num);
+                num = check_num;
+            }
+
+            for (int i = 0; i < num; i++)
+            {
+                test[i].Re = i;
+            }
         }
+        else if (order == 'n')
+        {
+            double real, imag;
+            printf("Please Enter the Point numbers you want?\n>");
+            scanf("%d", &num);
+            c = getchar();
+            check_num = num;
+            while ((check_num & (check_num - 1)))
+            {
+                check_num++;
+            }
+            if (num != check_num)
+            {
+                printf("The number you input is not 2^n , so I fill it to satisfy 2^n\nthe num will be %d", check_num);
+                num = check_num;
+            }
+
+            for (int i = 0; i < num; ++i)
+            {
+                printf("Input the REAL part of [%d] : ", i);
+                scanf("%lf", &real);
+                c = getchar();
+                test[i].Re = real;
+                printf("Input the Imag part of [%d] : ", i);
+                scanf("%lf", &imag);
+                c = getchar();
+                test[i].Im = imag;
+            }
+        }
+
+        // printf("Please Enter the Point numbers you want?\n>");
+        // scanf("%d", &num);
+        // check_num = num;
+        // while ((check_num & (check_num - 1)))
+        // {
+        //     check_num++;
+        // }
+        // num = check_num;
+
+        // for (int i = 0; i < num; i++)
+        // {
+        //     test[i].Re = i;
+        // }
         printf("===========================================\n");
         printf("Before FFT\n");
         for (int i = 0; i < num; ++i)
@@ -52,8 +108,7 @@ int main(int argc, char **argv)
             printf("[ %d ]\t%lf\t%lf\n", i, test[i].Re, test[i].Im);
         }
         printf("===========================================\n\n\n");
-        while ((c = getchar()) != EOF && c != '\n')
-            ;
+        free(test);
     }
 
     return 0;
