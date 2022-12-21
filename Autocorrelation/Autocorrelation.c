8a8eb8b4382a51f6a42b45e58c08084779d9ea9d
WangHanChi <hank1122334455667788990@gmail.com>
2022-12-21 14:51:26 +0800

    Add auto

--- Autocorrelation/Autocorrelation.c
+++ Autocorrelation/Autocorrelation.c
@@ -6,6 +6,7 @@
 #define NUM 32
 
 void auto_direct(double *, double *);
+void auto_DRFFT64(Complex *, Complex *);
 
 int main(int argc, char **argv)
 {
@@ -23,9 +24,56 @@ int main(int argc, char **argv)
     {
         printf("R[%3d]\t:%lf\n", i - 32, auto_out[i]);
     }
-
+    printf("\n\n(b)\n");
     printf("A direct real autocorrecttion computation program\n\n");
+
+    Complex *x2 = malloc(sizeof(Complex) * 32);
+    Complex *auto_out2 = calloc(sizeof(Complex), 64);
+    for (int i = 0; i < 32; i++)
+    {
+        x2[i].Re = (i + 1) % 2 ? -(i + 1) : (i + 1);
+    }
+    x[0] = 0;
+    auto_DRFFT64(auto_out2, x2);
+    for (int i = 0; i < 64 - 1; i++)
+    {
+        printf("R[%3d]\t:%lf\n", i - 31, auto_out2[i].Re);
+    }
     return 0;
+}
+
+void auto_DRFFT64(Complex *R, Complex *x)
+{
+    x = realloc(x, sizeof(Complex) * 64);
+    for (int i = 32; i < 64; i++)
+    {
+        x[i].Re = 0;
+        x[i].Im = 0;
+    }
+    Complex *h = calloc(sizeof(Complex), 64);
+    for (int i = 0; i < 32; i++)
+        h[i].Re = x[31 - i].Re;
+
+    DRFFT64(x, h);
+
+    // Complex *result = calloc(sizeof(Complex), 64);
+
+    for (int i = 0; i < 64; i++)
+    {
+        complex_mul(x[i], h[i], &R[i]);
+    }
+
+    IFFT64(R);
+
+    // for (int i = 0; i < 64; i++)
+    // {
+    //     printf("out2[ %d ]:\t %lf     \t%lf\n", i, result[i].Re, result[i].Im);
+    // }
+
+    // double sum = 0;
+    // for (int k = 0; k < 64; k++)
+    // {
+    // }
 }
 
 void auto_direct(double *R, double *x)
